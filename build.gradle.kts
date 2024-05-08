50cd9b3f04d4ae9ad3e192974d23eac364b8b020
Jess Yu <learningen13@gmail.com>
2024-05-09 00:37:14 +0800

    Set the basic environment for Google Maps API

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    // JSON API
+    implementation ("org.json:json:20240303")
+    // Google Map API
+    implementation ("com.google.maps:google-maps-services:2.2.0")
+    implementation ("org.slf4j:slf4j-simple:1.7.25")
 }
-
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+}
 tasks.test {
     useJUnitPlatform()
 }
--- src/main/java/com/whattoeat/Main.java
+++ src/main/java/com/whattoeat/Main.java
@@ -1,7 +1,42 @@
 package com.whattoeat;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import com.google.maps.errors.ApiException;
+import com.google.maps.model.LatLng;
+import com.google.maps.model.PlacesSearchResponse;
+import com.google.maps.model.PlacesSearchResult;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import com.google.maps.GeoApiContext;
+import com.google.maps.NearbySearchRequest;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, InterruptedException, ApiException {
+        // Get Google Map API key
+        String apiKey = "";
+        try {
+            FileReader reader = new FileReader("src/env.json");
+            JSONTokener tokener = new JSONTokener(reader);
+            JSONObject json = new JSONObject(tokener);
+            apiKey = json.getString("key");
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        int radius = 1000;
+        LatLng location = new LatLng(22.997140, 120.216211);
+        String keyWord = "restaurant";
+        GeoApiContext context = new GeoApiContext.Builder().apiKey(apiKey).build();
+        NearbySearchRequest request = new NearbySearchRequest(context).
+                keyword(keyWord).radius(radius).location(location).language("zh-TW");
+        PlacesSearchResponse response = request.await();
+        for(PlacesSearchResult result : response.results) {
+            System.out.println(result.name + "  " + result.rating);
+        }
     }
 }
