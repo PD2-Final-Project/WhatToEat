9e9e8f69e7254f85888a88819745880bcb85d723
Jess Yu <learningen13@gmail.com>
2024-05-13 23:51:18 +0800

    Finished the basic structure of weighted functions.

--- src/main/java/com/whattoeat/model/processor/DataProcessor.java
+++ src/main/java/com/whattoeat/model/processor/DataProcessor.java
@@ -17,7 +17,6 @@ import java.util.HashMap;
 public class DataProcessor {
     private JSONArray searchResults;
     private int priceLevel = Integer.valueOf(PriceLevel.MODERATE.toString());
-    private int[] radius;
     private double rating = 3;
     private int mood = Mood.CALM.getMoodCode();
     // The value of weight scales from 0 to 10
@@ -25,34 +24,29 @@ public class DataProcessor {
     private int distanceWeight = 5;
     private int ratingWeight = 5;
     private int moodWeight = 5;
-
     public DataProcessor(JSONArray searchResults) {
         this.searchResults = searchResults;
-        this.radius = new int[searchResults.length()];
-        for(int i = 0 ; i < searchResults.length() ; i++) {
-            this.radius[i] = searchResults.getJSONObject(i).getInt("radius");
-        }
     }
 
-    public JSONArray getWeightedSearchResults() {
-        JSONArray weightedSearchResults = new JSONArray();
-        // This will process each search results
-        for(int i = 0 ; i < searchResults.length() ; i++) {
-            // The data that won't affect the results
-            JSONObject searchResult = searchResults.getJSONObject(i);
-            JSONObject weightedSearchResult = new JSONObject();
-            weightedSearchResult.put("location", searchResult.getString("location"));
-            weightedSearchResult.put("keyword", searchResult.getString("keyword"));
-            weightedSearchResult.put("radius", searchResult.getString("radius"));
-            // To process each stores in the storeContent array and store it to weightedSearchResult
+    private void setStandardizedDistanceValue(){
+        for(int i = 0 ; i < this.searchResults.length() ; i++){
+            JSONObject searchResult = this.searchResults.getJSONObject(i);
             JSONArray storeContent = searchResult.getJSONArray("storeContent");
-            weightedSearchResult.put("storeContent", weightedSearchResultCalculator(storeContent, this.radius[i]));
-            weightedSearchResults.put(weightedSearchResult);
+            double[] distanceValues = new double[storeContent.length()];
+            for(int j = 0 ; j < storeContent.length() ; j++) {
+                JSONObject details = storeContent.getJSONObject(j).getJSONObject("details");
+                distanceValues[j] = details.getDouble("distance");
+            }
+            DataStandardization ds = new DataStandardization(distanceValues);
+            ds.setScale(5);
+            double[] standardizedDistanceValues = ds.getStandardizedValues();
+            for(int j = 0 ; j < standardizedDistanceValues.length ; j++) {
+                storeContent.getJSONObject(j).getJSONObject("details").put("distance", standardizedDistanceValues[j]);
+            }
         }
-        return weightedSearchResults;
     }
 
-    private JSONArray weightedSearchResultCalculator(JSONArray storeContent, int radius){
+    private JSONArray weightedSearchResultCalculator(JSONArray storeContent){
         JSONArray weightedStoreContent = new JSONArray();
         ArrayList<JSONObject> storeDetails = new ArrayList<JSONObject>();
         for(int i = 0 ; i < storeContent.length() ; i++) {
@@ -65,8 +59,8 @@ public class DataProcessor {
                 o2 = o2.getJSONObject("details");
                 int priceLevel1 = 6 - o1.getInt("priceLevel");
                 int priceLevel2 = 6 - o2.getInt("priceLevel");
-                int distance1 = o1.getInt("distance")/radius*5;
-                int distance2 = o2.getInt("distance")/radius*5;
+                int distance1 = o1.getInt("distance");
+                int distance2 = o2.getInt("distance");
                 double rating1 = o1.getDouble("rating");
                 double rating2 = o2.getDouble("rating");
 
@@ -76,10 +70,7 @@ public class DataProcessor {
                 double weight2 = priceLevel2 * DataProcessor.this.priceLevelWeight +
                         distance2 * DataProcessor.this.distanceWeight +
                         rating2 * DataProcessor.this.ratingWeight;
-                if(weight1 > weight2 || weight1 == weight2) {
-                    return 1;
-                }
-                return -1;
+                return Double.compare(weight2, weight1);
             }
         });
         for(int i = 0 ; i < storeDetails.size() ; i++) {
@@ -88,13 +79,32 @@ public class DataProcessor {
         return weightedStoreContent;
     }
 
-    public void setPriceLevel(PriceLevel priceLevel) {
-        this.priceLevel = Integer.parseInt(priceLevel.toString());
+    public JSONArray getWeightedSearchResults() {
+        this.setStandardizedDistanceValue();
+        JSONArray weightedSearchResults = new JSONArray();
+        // This will process each search results
+        for(int i = 0 ; i < searchResults.length() ; i++) {
+            // The data that won't affect the results
+            JSONObject searchResult = searchResults.getJSONObject(i);
+            JSONObject weightedSearchResult = new JSONObject();
+            weightedSearchResult.put("location", searchResult.getString("location"));
+            weightedSearchResult.put("keyword", searchResult.getString("keyword"));
+            weightedSearchResult.put("radius", searchResult.getString("radius"));
+            // To process each stores in the storeContent array and store it to weightedSearchResult
+            JSONArray storeContent = searchResult.getJSONArray("storeContent");
+            weightedSearchResult.put("storeContent", weightedSearchResultCalculator(storeContent));
+            weightedSearchResults.put(weightedSearchResult);
+        }
+        return weightedSearchResults;
     }
 
 //    public void setRadius(int radius, int index) {
 //        this.radius[index] = radius;
 //    }
+
+    public void setPriceLevel(PriceLevel priceLevel) {
+        this.priceLevel = Integer.parseInt(priceLevel.toString());
+    }
 
     public void setRating(double rating) {
         this.rating = rating;
---
+++ src/main/java/com/whattoeat/model/processor/MinMaxNormalization.java
@@ -0,0 +1,21 @@
+package com.whattoeat.model.processor;
+
+public class MinMaxNormalization {
+    public static double[] minMaxNormalize(double[] values, int scale) {
+        double[] normalized = new double[values.length];
+        double xmin = values[0];
+        double xmax = values[0];
+        for (int i = 1; i < values.length; i++) {
+            if(values[i] < xmin) {
+                xmin = values[i];
+            }
+            if(values[i] > xmax) {
+                xmax = values[i];
+            }
+        }
+        for(int i = 0 ; i < values.length ; i ++) {
+            normalized[i] = ((values[i] - xmin) / (xmax - xmin)) * scale;
+        }
+        return normalized;
+    }
+}
